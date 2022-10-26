33496c44bd6a7a745645317a84528ecaee689f23
d3rpp <hudson.curren@icloud.com>
2022-10-26 13:31:50 +1300

    optimised a lot of the classnames

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
+let hashmap = {};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -19,6 +21,18 @@ const config = {
 		},
 
 		trailingSlash: 'always',
+	},
+
+	compilerOptions: {
+		cssHash: ({ name, filename, css, hash }) => {
+			let content_hash = hash(`${name}${filename}${css}`);
+
+			if (!hashmap[content_hash]) {
+				hashmap[content_hash] = Object.keys(hashmap).length.toString(16);
+			}
+
+			return `d${hashmap[content_hash]}`;
+		}
 	}
 };
 
